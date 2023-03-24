7dcfa109c70af893170f2f05bbccdb7f472508b2
Bob K <rwkramer@ysu.edu>
2023-03-23 23:06:35 -0400

    starter for project 3

---
+++ Projects/Project 3/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "pathfinder.h"
+
+uint8_t
+    maze[MAX_ROWS][MAX_COLS];
+
+void generateTreeMaze(uint32_t nRows,uint32_t nCols) {
+
+}
+
+void removeAdditionalWalls(uint32_t nRows,uint32_t nCols,uint32_t nWalls) {
+
+}
+
+void findPath(uint32_t nRows,uint32_t nCols) {
+
+}
+
+int main(int argc,char *argv[]) {
+
+    // make sure command line is proper
+
+    // get r, c, w from command line
+
+    // generate maze
+
+    // solve maze
+
+    // print the maze
+
+    return 0;
+}
